109ba4cfe8fdc78ee6be47d4dbf262fb94949da3
Lakshit <lakshitbarak188@gmail.com>
2026-06-03 17:18:23 +0530

    day 3

---
+++ Day_3/Problem_66.java
@@ -0,0 +1,17 @@
+package Day_3;
+
+public class Problem_66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
